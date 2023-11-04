7ef43634d32121fee81f3b35989e65f44e2e9c09
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-11-04 11:02:54 -0400

    added script

---
+++ script.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('[data-smart-section]');
+    let lastVisibleSectionIndex = null;
+    let ticking = false; // flag to use with requestAnimationFrame
+  
+    // Cache values that are unchanged during scroll events
+    const sectionDataValues = Array.from(sections).map(section => section.dataset.smartSection);
+    const contextItemsMap = new Map();
+  
+    // Precompute and store contextItems in a Map for O(1) access
+    document.querySelectorAll('[data-context-item]').forEach(item => {
+      contextItemsMap.set(item.dataset.contextItem, item);
+    });
+  
+    function getVisibilityPercentage(element) {
+      const rect = element.getBoundingClientRect();
+      const totalHeight = window.innerHeight || document.documentElement.clientHeight;
+      const visibleArea = Math.min(rect.bottom, totalHeight) - Math.max(rect.top, 0);
+      const elementHeight = rect.bottom - rect.top;
+      return Math.max(0, Math.min(visibleArea / elementHeight, 1));
+    }
+  
+    function updateActiveItem() {
+      let mostVisibleSectionIndex = null;
+      let highestVisibility = 0;
+  
+      // Determine the most visible section
+      sectionDataValues.forEach((dataValue, index) => {
+        const visibility = getVisibilityPercentage(sections[index]);
+        if (visibility > highestVisibility && visibility > 0.2) {
+          highestVisibility = visibility;
+          mostVisibleSectionIndex = index;
+        }
+      });
+  
+      // Only update if the most visible section has changed
+      if (mostVisibleSectionIndex !== lastVisibleSectionIndex) {
+        if (lastVisibleSectionIndex !== null) {
+          const lastDataValue = sectionDataValues[lastVisibleSectionIndex];
+          contextItemsMap.get(lastDataValue).classList.remove('is-active');
+        }
+  
+        lastVisibleSectionIndex = mostVisibleSectionIndex;
+  
+        if (mostVisibleSectionIndex !== null) {
+          const newDataValue = sectionDataValues[mostVisibleSectionIndex];
+          contextItemsMap.get(newDataValue).classList.add('is-active');
+        }
+      }
+    }
+  
+    function throttledUpdateActiveItem() {
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          updateActiveItem();
+          ticking = false;
+        });
+        ticking = true;
+      }
+    }
+  
+    // Listen for the scroll event
+    window.addEventListener('scroll', throttledUpdateActiveItem);
+  
+    // Initialize on page load
+    updateActiveItem();
+  });
